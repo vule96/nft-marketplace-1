209aec5770866a463ae9645d1cd88ea29f29fb3c
Vu Le <vu.le@successsoftware.global>
2022-05-11 17:35:33 +0700

    Added metamask provider and ethers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,13 @@
+import { Web3Provider } from "components/providers";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
+      <Web3Provider>
+        <Component {...pageProps} />
+      </Web3Provider>
     </>
   );
 }
